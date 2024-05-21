45fea603d36bc396692eadf08363edf86107381e
Natan Młynarczyk <nnatiimlynarczyknatan@gmail.com>
2024-05-21 17:19:14 +0200

    dziala

--- SFMLGameProject/SFMLGameProject.cpp
+++ SFMLGameProject/SFMLGameProject.cpp
@@ -1,9 +1,21 @@
-﻿
+﻿#include "SFML/Graphics.hpp" // w tym miejscu nawiasy ostre (youtube uniemożliwia umieszczenie ich w opisie filmu)
 
-#include <iostream>
+int main(void) {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main()
-{
-    std::cout << "Git working yes/no dodawane z gita\n";
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
-
